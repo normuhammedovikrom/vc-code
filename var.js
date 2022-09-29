0623e604b3a79da7df15d72d7c2514856841efb2
normuhammedovikrom <normuhammedovikrom4@gmail.com>
2022-09-29 06:58:47 +0500

    vc code

---
+++ var.js
@@ -0,0 +1,35 @@
+let div = document.querySelector(".pro");
+
+div.addEventListener("click", () => {
+    div.classList.toggle("box")
+})
+
+let page = document.querySelector(".pages");
+
+page.addEventListener("click" , () => {
+    page.classList.toggle("box")
+})
+
+let table = document.querySelector(".table");
+
+table.addEventListener("click" , () => {
+    table.classList.toggle("box")
+})
+
+let form = document.querySelector(".form");
+
+form.addEventListener("click" , () => {
+    form.classList.toggle("box")
+})
+
+let item = document.querySelector(".item");
+
+item.addEventListener("click" , () => {
+    item.classList.toggle("box")
+})
+
+let map = document.querySelector(".map");
+
+map.addEventListener("click" , () => {
+    map.classList.toggle("box")
+})
